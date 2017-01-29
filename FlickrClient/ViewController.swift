699aea239dd167bb4da7f6eb03d2c1bf2fb24777
Ömer Aslan <omer.aslan@ozu.edu.tr>
2017-01-29 15:06:10 +0300

    Init xcode project

---
+++ FlickrClient/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  FlickrClient
+//
+//  Created by Omer on 29/01/2017.
+//  Copyright © 2017 Omer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = UIColor.white
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
